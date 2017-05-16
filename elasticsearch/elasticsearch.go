aaf231a964c60055b8f844287ad444ecae993f78
ryusei <ryuseitakeda@gmail.com>
2017-05-16 13:58:56 +0900

    Add Elastic client

---
+++ elasticsearch/elasticsearch.go
@@ -0,0 +1,25 @@
+package elasticsearch
+
+import (
+	"context"
+
+	"gopkg.in/olivere/elastic.v5"
+)
+
+// Contextの初期化とelasticsearchのコネクションを確立します。
+func Elastic() (*elastic.Client, context.Context) {
+	ctx := context.Background()
+
+	client, err := elastic.NewClient(
+		// elasticsearchのdocker内IPアドレス
+		elastic.SetURL("elasticsearch:9200"),
+		// これつけないとつながらない?
+		// https://github.com/olivere/elastic/issues/58#issuecomment-156052782
+		elastic.SetSniff(false),
+	)
+	if err != nil {
+		// Handle error
+		panic(err)
+	}
+	return client, ctx
+}
