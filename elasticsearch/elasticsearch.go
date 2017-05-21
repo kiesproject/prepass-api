d26af1c49d4fea28305002cf12f36bd46ba598b0
ryusei <ryuseitakeda@gmail.com>
2017-05-21 19:19:09 +0900

    DockerのためにつけてたURL指定を外した

--- elasticsearch/elasticsearch.go
+++ elasticsearch/elasticsearch.go
@@ -10,13 +10,7 @@ import (
 func Elastic() (*elastic.Client, context.Context) {
 	ctx := context.Background()
 
-	client, err := elastic.NewClient(
-		// elasticsearchのdocker内IPアドレス
-		elastic.SetURL("127.0.0.1:9200"),
-		// これつけないとつながらない?
-		// https://github.com/olivere/elastic/issues/58#issuecomment-156052782
-		elastic.SetSniff(false),
-	)
+	client, err := elastic.NewClient()
 	if err != nil {
 		// Handle error
 		panic(err)
